d617aebb1258f833128957b64d1eb72f86bb6798
roboshop <j.devops05324u@gmail.com>
2024-11-09 10:18:58 +0530

    modify in ci file

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -2,12 +2,19 @@ def call () {
   
   pipeline {
     agent any
-      stages {
-        stage('compile') {
-          steps {
-             script {
-               common.compile()
-	     }
+       stages {
+          stage('compile') {
+            steps {
+              script {
+                common.compile()
+	          }
+           }
+         }
+         stage('unit test') {
+           steps {
+             script{
+               common.unittest()
+            }
           }
         }
      }
--- vars/common.groovy
+++ vars/common.groovy
@@ -1,5 +1,20 @@
 def compile() {
- if(app_lang=="nodejs") {
-    sh 'npm install'
- }
+    if (app_lang == "nodejs") {
+        sh 'npm install'
+    }
+    if (app_lang == "java") {
+        sh 'mvn clean package'
+    }
 }
+def unittest(){
+    if(app_lang=="nodejs"){
+        sh 'npm test || true'
+    }
+    if(app_lang=="java"){
+        sh 'mvn test'
+    }
+    if(app_lang=="python"){
+        sh 'python3 -m unittest'
+    }
+}
+
