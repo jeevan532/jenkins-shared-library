ae8391acef6df8efbf4e148156427e9928873bda
roboshop <j.devops05324u@gmail.com>
2024-11-13 15:44:18 +0530

    scripted pipeline

---
+++ vars/s-ci.groovy
@@ -0,0 +1,23 @@
+def call () {
+    try {
+        node ('workstation') {
+            stage ('clean workspace') {
+                cleanWs()
+            }
+            stage('compile/build') {
+                common.compile()
+            }
+            stage ('unittest') {
+                common.unittest()
+            }
+            stage ('quality control') {
+                sonar_user = sh (script: 'aws --region=us-east-1 ssm get-parameter --name "sonar.user" --with-decryption --output text --query Parameter.Value', returnStdout: true).trim()
+                sonar_password = sh (script: 'aws --region=us-east-1 ssm get-parameter --name "sonar.password" --with-decryption --output text --query Parameter.Value', returnStdout: true).trim()
+                sh " sonar-scanner -X -Dsonar.host.url=http://172.31.43.149:9000 -Dsonar.login=admin -Dsonar.password=Jeevan@05324u -Dsonar.projectKey=${component} "
+            }
+        }
+    }  catch (Exception e) {
+       common.email(e,'failed')
+    }
+}
+
