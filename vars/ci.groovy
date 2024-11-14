84b51f4ec8059fe27b8a0d9e9dbf7f680e6bd5a9
roboshop <j.devops05324u@gmail.com>
2024-11-14 10:54:20 +0530

    scripted pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -13,7 +13,7 @@ def call () {
             stage ('quality control') {
                 sonar_user = sh (script: 'aws --region=us-east-1 ssm get-parameter --name "sonar.user" --with-decryption --output text --query Parameter.Value', returnStdout: true).trim()
                 sonar_password = sh (script: 'aws --region=us-east-1 ssm get-parameter --name "sonar.password" --with-decryption --output text --query Parameter.Value', returnStdout: true).trim()
-                sh " sonar-scanner -X -Dsonar.host.url=http://172.31.43.149:9000 -Dsonar.login=admin -Dsonar.password=Jeevan@05324u -Dsonar.projectKey=${component} "
+                sh " sonar-scanner -Dsonar.host.url=http://172.31.43.149:9000 -Dsonar.login=admin -Dsonar.password=Jeevan@05324u -Dsonar.projectKey=${component} "
             }
         }
     }  catch (Exception e) {
