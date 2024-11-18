52f30462e882201a7d30f883f3d2e0dc03e3402e
roboshop <j.devops05324u@gmail.com>
2024-11-18 12:52:28 +0530

    scripted pipeline

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -13,11 +13,12 @@ def call () {
             stage ('quality control') {
                 sonar_user = sh (script: 'aws --region=us-east-1 ssm get-parameter --name "sonar.user" --with-decryption --output text --query Parameter.Value', returnStdout: true).trim()
                 sonar_password = sh (script: 'aws --region=us-east-1 ssm get-parameter --name "sonar.password" --with-decryption --output text --query Parameter.Value', returnStdout: true).trim()
-                sh " sonar-scanner -Dsonar.host.url=http://172.31.43.149:9000 -Dsonar.login=admin -Dsonar.password=Jeevan@05324u -Dsonar.projectKey=${component} "
+                wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: ${sonar_password}]]]) {
+                sh " sonar-scanner -Dsonar.host.url=http://172.31.43.149:9000 -Dsonar.login=admin -Dsonar.password=Jeevan@05324u -Dsonar.projectKey=${component} ${extra_opt}"
             }
         }
-    }  catch (Exception e) {
-//       common.email(e,'failed')
+    } catch (Exception e) {
+       common.email(e,'failed')
         echo "test failed"
     }
 }
